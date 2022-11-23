d2988e8a04a8583dba9fb6e0ae0e196721d5d183
sridatae <94915326+sridatae@users.noreply.github.com>
2022-11-22 22:24:01 -0500

    Add files via upload

--- src/action_page.php
+++ src/action_page.php
@@ -1,27 +1,27 @@
 <?php
- $fieldA = $_POST["date"];
- $fieldB = $_POST["cname"];
- $fieldC = $_POST["address"];
- $fieldD = $_POST["fcode"];
- $fieldE = $_POST["qnty"];
- $fieldF = $_POST["qntp"];
- $fieldG = $_POST["fname"];
- $fieldH = $_POST["prvcus"];
- $fieldI = $_POST["slrp"];
- $fieldJ = $_POST["pmd"];
- $fieldK = $_POST["invsq"];
 
- $keys = array($fieldA,$fieldB,$fieldC,$fieldD,$fieldE,$fieldF,$fieldG,$fieldH,$fieldI,$fieldJ,$fieldK); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
- $csv_line = $keys;
- foreach( $keys as $key ){
-     array_push($csv_line,'' . $_GET[$key]);
+// Create an array of elements
+$fieldA = $_POST["date"];
+$fieldB = $_POST["cname"];
+$fieldC = $_POST["address"];
+$fieldD = $_POST["fcode"];
+$fieldE = $_POST["qnty"];
+$fieldF = $_POST["qntp"];
+$fieldG = $_POST["fname"];
+$fieldH = $_POST["prvcus"];
+$fieldI = $_POST["slrp"];
+$fieldJ = $_POST["pmd"];
+$fieldK = $_POST["invsq"];
 
- }
- $fname = 'data.csv'; //NAME OF THE FILE
- $csv_line = implode(',',$csv_line);
- if(!file_exists($fname)){$csv_line = $csv_line."\r\n" ;}
- $fcon = fopen($fname,'a');
- $fcontent = $csv_line;
- fwrite($fcon,$csv_line);
- fclose($fcon);
- ?>
+$list = array([$fieldA,$fieldB,$fieldC,$fieldD,$fieldE,$fieldF,$fieldG,$fieldH,$fieldI,$fieldJ,$fieldK]); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
+
+// Open a file in write mode ('a')
+$fp = fopen('persons.csv', 'a');
+
+// Loop through file pointer and a line
+foreach ($list as $fields) {
+fputcsv($fp, $fields);
+}
+
+fclose($fp);
+?>
