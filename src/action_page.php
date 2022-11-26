a1813a0ab1c8108ba21b9ce09f20f7fa50391bd1
sridatae <94915326+sridatae@users.noreply.github.com>
2022-11-26 13:50:18 -0500

    Add files via upload

--- src/action_page.php
+++ src/action_page.php
@@ -1,20 +1,21 @@
 <?php
 
 // Create an array of elements
-$fieldA = $_POST["date"];
-$fieldB = $_POST["cname"];
-$fieldC = $_POST["address"];
-$fieldD = $_POST["fcode"];
-$fieldE = $_POST["qnty"];
-$fieldF = $_POST["qntp"];
-$fieldG = $_POST["fname"];
-$fieldH = $_POST["prvcus"];
-$fieldI = $_POST["slrp"];
-$fieldJ = $_POST["pmd"];
-$fieldK = $_POST["invsq"];
+// $fieldA = $_POST["date"];
+// $fieldB = $_POST["cname"];
+// $fieldC = $_POST["address"];
+// $fieldD = $_POST["fcode"];
+// $fieldE = $_POST["qnty"];
+// $fieldF = $_POST["qntp"];
+// $fieldG = $_POST["fname"];
+// $fieldH = $_POST["prvcus"];
+// $fieldI = $_POST["slrp"];
+// $fieldJ = $_POST["pmd"];
+// $fieldK = $_POST["invsq"];
+$fieldL = $_POST["info22"];
 
-$list = array([$fieldA,$fieldB,$fieldC,$fieldD,$fieldE,$fieldF,$fieldG,$fieldH,$fieldI,$fieldJ,$fieldK]); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
-
+// $list = array([$fieldA,$fieldB,$fieldC,$fieldD,$fieldE,$fieldF,$fieldG,$fieldH,$fieldI,$fieldJ,$fieldK,$fieldL]); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
+$list= array([$fieldL]);
 // Open a file in write mode ('a')
 $fp = fopen('persons.csv', 'a');
 
